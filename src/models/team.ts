eb7b82fd32bcce6ca6f82c647e42439d7ee0f1af
Yorukot <ben.hrc57@gmail.com>
2025-01-28 00:57:18 +0800

    feat: finish create new team api endpoint

--- src/models/team.ts
+++ src/models/team.ts
@@ -8,11 +8,19 @@ export interface Team {
   _id: string; // uuidv4
   team_name: string;
   team_size: number;
-  members_id?: Array<string>;
+  leader_id: string;
+  teacher_id: string;
+  members_id: Array<string>;
   createdAt?: Date;
   updatedAt?: Date;
 
   ignore_encryption: IgnoreEncryption;
+}
+
+export interface TeamLink extends Team {
+  leader_link: string;
+  members_link: Array<string>;
+  teacher_link: string;
 }
 
 export const defaultIgnoreEncryption: IgnoreEncryption = {
