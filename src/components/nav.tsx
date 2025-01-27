e845cee08f59f0df6e195836aff8e537b2e1dbed
wolf-yuan <wolf@wolf-yuan.dev>
2025-01-27 17:45:37 +0800

    feat: fix navbar color and padding issue

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,18 +4,16 @@ import NavThemeToggle from "@/components/navThemeToggle";
 
 export default function () {
   return (
-    <nav className="sticky z-10 top-0 left-0 w-full bg-black/50">
-      <div className="max-w-6xl px-4 py-2 grid grid-cols-2">
-        <div className="justify-start items-center flex">
-          <Image
-            src={favicon}
-            alt="Scrapyard signup system favicon"
-            width={48}
-            height={48}
-          />
-          <h1 className="ml-4 font-semibold">Scrapyard 報名系統</h1>
-        </div>
-        <div className="justify-end items-center flex">
+    <nav className="sticky z-10 top-0 left-0 w-full bg-gray-300 dark:bg-gray-700 ">
+      <div className="mx-auto max-w-6xl px-4 py-2 flex place-items-center ">
+        <Image
+          src={favicon}
+          alt="Scrapyard signup system favicon"
+          width={38}
+          height={38}
+        />
+        <h1 className="ml-4 font-semibold">Scrapyard 報名系統</h1>
+        <div className="ml-auto">
           <NavThemeToggle />
         </div>
       </div>
