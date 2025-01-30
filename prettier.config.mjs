1ce993ebe7a9bd5ea4300c7fe81c7ec50bc4411f
wolf-yuan <wolf@wolf-yuan.dev>
2025-01-30 14:46:34 +0800

    chore: add prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+};
