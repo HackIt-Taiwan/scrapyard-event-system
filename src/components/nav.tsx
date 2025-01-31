9aa980b1a5a5e96f75e427205b44f2a327aa78d4
wolf-yuan <wolf@wolf-yuan.dev>
2025-01-31 16:51:31 +0800

    feat: let nav image clickable

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -7,15 +7,16 @@ export default function () {
   return (
     <nav className="sticky left-0 top-0 z-10 w-full bg-gray-300 dark:bg-gray-700">
       <div className="mx-auto flex max-w-6xl place-items-center px-4 py-2">
-        <Image
-          src={favicon}
-          alt="Scrapyard signup system favicon"
-          width={38}
-          height={38}
-        />
-        <h1 className="ml-4 font-semibold">
-          <Link href="/">Scrapyard 報名系統</Link>
-        </h1>
+        <Link href="/">
+          <Image
+            src={favicon}
+            alt="Scrapyard signup system favicon"
+            width={38}
+            height={38}
+            className="inline"
+          />
+          <h1 className="ml-4 inline font-semibold">Scrapyard 報名系統</h1>
+        </Link>
         <div className="ml-auto">
           <NavThemeToggle />
         </div>
