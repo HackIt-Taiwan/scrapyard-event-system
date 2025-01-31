5f84cd1dd6c005e2494c3a6d7c6a0098c730689f
wolf-yuan <wolf@wolf-yuan.dev>
2025-01-31 16:50:25 +0800

    fix: main page broken after fixing step 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mx-auto flex grow flex-col place-items-center justify-center">
       <Image
         src={banner}
         alt="Scrapyard 橫幅"
-        className="mb-6 h-44 w-fit"
+        className="mb-12 h-44 w-fit"
         width={600}
         height={286}
       />
@@ -17,11 +17,11 @@ export default function Home() {
       <h1 className="mb-2 text-center text-2xl font-bold md:text-3xl">
         參加 Scrapyard 黑客松
       </h1>
-      <h2 className="mb-4 text-center">
+      <h2 className="mb-6 text-center">
         Scrapyard 是由 HackIt 團隊舉辦的 Hack Club 黑客松
       </h2>
 
-      <Button variant="secondary" className="w-full" asChild>
+      <Button variant="default" className="w-full" asChild>
         <Link href="/apply/steps/1">立即報名</Link>
       </Button>
     </div>
