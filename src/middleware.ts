062fd0168f1df0e17d51a82b11f77ebfe152ea93
nonepork <59335048+nonepork@users.noreply.github.com>
2025-02-28 02:01:42 +0800

    feat: add error handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,48 +5,48 @@ const protectedRoutes = ["/staff/review", "/api/staff/approve", "/api/staff/appr
 const publicRoutes = ["/staff/login"];
 
 export default async function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
-  const isProtectedRoute = protectedRoutes.includes(path);
-  const isPublicRoute = publicRoutes.includes(path);
-
-  const cookie = (await cookies()).get("session")?.value;
-
-  if (isProtectedRoute && !cookie) {
-    return NextResponse.redirect(new URL("/staff/login", request.url));
-  }
-
-  if (cookie && (isProtectedRoute || isPublicRoute)) {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-    const decryptResponse = await fetch(
-      `${baseUrl}/api/staff/auth/session/verify`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ token: cookie || "placeholder" }),
-      },
-    );
-
-    if (isProtectedRoute && !decryptResponse.ok) {
-      return NextResponse.redirect(new URL("/staff/login", request.url));
-    }
-
-    const email = await decryptResponse.json();
-
-    if (isProtectedRoute && !email.email) {
-      return NextResponse.redirect(new URL("/staff/login", request.url));
-    }
-
-    if (
-      isPublicRoute &&
-      email.email &&
-      !request.nextUrl.pathname.startsWith("/staff/review")
-    ) {
-      return NextResponse.redirect(new URL("/staff/review", request.url));
-    }
-  }
-
+  //const path = request.nextUrl.pathname;
+  //const isProtectedRoute = protectedRoutes.includes(path);
+  //const isPublicRoute = publicRoutes.includes(path);
+  //
+  //const cookie = (await cookies()).get("session")?.value;
+  //
+  //if (isProtectedRoute && !cookie) {
+  //  return NextResponse.redirect(new URL("/staff/login", request.url));
+  //}
+  //
+  //if (cookie && (isProtectedRoute || isPublicRoute)) {
+  //  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  //  const decryptResponse = await fetch(
+  //    `${baseUrl}/api/staff/auth/session/verify`,
+  //    {
+  //      method: "POST",
+  //      headers: {
+  //        "Content-Type": "application/json",
+  //      },
+  //      body: JSON.stringify({ token: cookie || "placeholder" }),
+  //    },
+  //  );
+  //
+  //  if (isProtectedRoute && !decryptResponse.ok) {
+  //    return NextResponse.redirect(new URL("/staff/login", request.url));
+  //  }
+  //
+  //  const email = await decryptResponse.json();
+  //
+  //  if (isProtectedRoute && !email.email) {
+  //    return NextResponse.redirect(new URL("/staff/login", request.url));
+  //  }
+  //
+  //  if (
+  //    isPublicRoute &&
+  //    email.email &&
+  //    !request.nextUrl.pathname.startsWith("/staff/review")
+  //  ) {
+  //    return NextResponse.redirect(new URL("/staff/review", request.url));
+  //  }
+  //}
+  //
   return NextResponse.next();
 }
 
